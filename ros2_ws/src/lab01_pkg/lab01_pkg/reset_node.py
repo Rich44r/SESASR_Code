8ec3430f98d548a3e6afbd71a9fff7dcc99d48f7
Rich44r <richard1.lecca1@gmail.com>
2025-10-18 11:08:45 +0200

    TASK5: Aggiornati setup.py e package.xml, aggiunta di reset_node.py

---
+++ ros2_ws/src/lab01_pkg/lab01_pkg/reset_node.py
@@ -0,0 +1,41 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Bool
+from geometry_msgs.msg import Pose2D
+
+class ResetNode(Node):
+    def __init__(self):
+        super().__init__('ResetNode')
+        self.subscription = self.create_subscription(
+            Pose2D,
+            'pose',
+            self.listener_callback,
+            10)
+        self.counterx = 0
+        self.countery = 0
+        self.subscription  # prevent unused variable warning
+        self.publisher = self.create_publisher(Bool, 'reset', 10)
+        self.get_logger().info('ResetNode has been started.')
+
+    def listener_callback(self, msg):
+        self.get_logger().info(f'Received pose: {msg}')
+        self.counterx += msg.x
+        self.countery += msg.y
+        if self.counterx > 6 or self.countery > 6:
+            self.get_logger().info('Position out of bounds, RESET to (0,0)')
+            self.counterx = 0 #because the position will be reset to 0
+            self.countery = 0
+            self.publisher.publish(Bool(data=True))
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    reset_node = ResetNode()
+
+    rclpy.spin(reset_node)
+
+    reset_node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
