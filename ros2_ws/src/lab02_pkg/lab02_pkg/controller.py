ac5b8b694ce1c41f318b7b8a7e99c481d987d8c0
Pruitz <68857658+Pruitz@users.noreply.github.com>
2025-10-23 19:49:20 +0200

    Code done in the lab
    This is the code done in the lab02 23/10/2025

--- ros2_ws/src/lab02_pkg/lab02_pkg/controller.py
+++ ros2_ws/src/lab02_pkg/lab02_pkg/controller.py
@@ -1,41 +1,60 @@
 import rclpy
-import tf_transformations
 from rclpy.node import Node
+
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
-from nav_msgs.msg import Odometry
 
-
-LINEAR_VELOCITY = 0.20  # m/s
-ANGULAR_VELOCITY = 1.5  # rad/s
+Max_Vel = 0.22  # m/s
+Max_angVel = 1.5 #rad/s
 
 class ControllerNode(Node):
     def __init__(self):
-        super().__init__('Controller')
-        self.publisher = self.create_publisher(Twist, 'cmd_vel', 10)
-        self.subscription_scan = self.create_subscription(
+        super().__init__('controller_node')
+
+        #readin Lidar
+        self.subscription = self.create_subscription(     #IN a subscription, any time a msg is published on the topic the callback function is executed
             LaserScan,
             'scan',
-            self.laser_callback,
+            self.LidarScanner,
             10)
-        self.subscription_scan  
-        self.subscription_odom = self.create_subscription(
-            Odometry,
-            'odom',
-            self.odom_callback,
-            10)
-        self.subscription_odom  
-        self.get_logger().info('Controller node has been started.')
 
-    def laser_callback(self, msg):
+        self.velocity = Twist()
+   
+        self.publisher_ = self.create_publisher(Twist, 'cmd_vel', 10)
 
-        pass
+    def LidarScanner(self,scan):
+        self.velocity.angular.z = 0.0    
+        self.velocity.linear.x=0.0
         
+        if scan.ranges[0] < 0.75:
 
+            if scan.ranges[89] > scan.ranges[269]:
+                self.velocity.angular.z = Max_angVel-0.5
+            else:
+                self.velocity.angular.z = -Max_angVel-0.5
+        else: 
+            self.velocity.linear.x = Max_Vel
+            
+        if scan.ranges[29] < 0.6:
+            self.velocity.angular.z = -Max_angVel-0.5
+        elif scan.ranges[329] < 0.6: 
+            self.velocity.angular.z = Max_angVel-0.5
+            
+        self.publisher_.publish(self.velocity)
+        self.get_logger().info(f'Publishing Twist: linear.x={self.velocity.linear.x} angular.z={self.velocity.angular.z}')
+
+
+
+
+        
 def main(args=None):
-    rclpy.init(args=args)
-    controller_node = ControllerNode()
-    rclpy.spin(controller_node)
+    rclpy.init(args=args) #initializa ros python executer 
+
+    controller_node = ControllerNode()  
+
+    rclpy.spin(controller_node)   #the executro (rcply) has to spin the node (this is the way ros work)
+
+    # Destroy the node explicitly
     controller_node.destroy_node()
     rclpy.shutdown()
 
