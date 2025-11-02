b86f3031e84aa76c5a2b8137b9106236fc073e5d
Rich44r <richard1.lecca1@gmail.com>
2025-11-02 17:20:50 +0100

    angoli dinamici

--- ros2_ws/src/lab03_pkg/lab03_pkg/controller.py
+++ ros2_ws/src/lab03_pkg/lab03_pkg/controller.py
@@ -41,73 +41,40 @@ class ControllerNode(Node):
         self.subscriber_ground
         self.get_logger().info('Controller node has been started.')
 
-    def get_index(self,degree_target, angle_min, angle_increment):
-        #from degrees to rad
-        angle_target = math.radians(degree_target)
-        #calculate index
-        index = (angle_target-angle_min)/(angle_increment)
-        return int(round(index))
-
-
 
     def laser_callback(self, msg):
-       # L'errore è dovuto alla mancanza della tupla
-# PRIMA: self.get_logger().info('dimensione : %d', len(msg.ranges))
-
-# CORRETTO: Includi il valore in una tupla (o usa le f-string)
-        self.get_logger().info('dimensione : %d', (len(msg.ranges),))
+        self.get_logger().info(f'dimensione : {len(msg.ranges)}')
         distance_front = []
         distance_left = []
         distance_right = []
         send = Twist()
-        #calculate indices for front region
-        min1_front = self.get_index(0,msg.angle_min,msg.angle_increment)
-        max1_front = self.get_index(30,msg.angle_min,msg.angle_increment)
-        min2_front = self.get_index(330,msg.angle_min,msg.angle_increment)
-        max2_front = len(msg.ranges)-1 #last index
-        #front interval of 60 degrees
-        for i in range(min1_front,max1_front):
-            if not math.isinf(msg.ranges[i]):
-                distance_front.append(msg.ranges[i])
-            else:
-                distance_front.append(RANGE_MAX)  #assume no obstacle in that direction
-        for i in range(min2_front,max2_front):
-            if not math.isinf(msg.ranges[i]):
-                distance_front.append(msg.ranges[i])
-            else:
-                distance_front.append(RANGE_MAX)  #assume no obstacle in that direction
+        #list of real angles (angles avrà la stessa dimensione di ranges)
+        angles = [msg.angle_min + i*msg.angle_increment for i in range(len(msg.ranges))]
+        #interval definitions
+        min1_front, max1_front = math.radians(0), math.radians(30)
+        min2_front, max2_front = math.radians(330), math.radians(360)
+        min_left, max_left = math.radians(30), math.radians(90)
+        min_right, max_right = math.radians(270), math.radians(330)
 
+        #d : distance, a : angle
+        distance_front = [d if not math.isinf(d) else RANGE_MAX
+                          for d,a in zip(msg.ranges, angles) if min1_front<=a<= max1_front or min2_front<=a<=max2_front]
+        distance_left = [d if not math.isinf(d) else RANGE_MAX
+                         for d,a in zip(msg.ranges, angles) if min_left <= a<= max_left]
+        distance_right = [d if not math.isinf(d) else RANGE_MAX
+                         for d,a in zip(msg.ranges, angles) if min_right <= a<= max_right]
+        
         if min(distance_front)>MINIMUM_DISTANCE:
             #self.get_logger().info('moving forward')
-            send.linear.x = LINEAR_VELOCITY
+            self.state = "forward"
             distance_front.clear()
         else:
             send.linear.x = 0.0
-            #calculate left and right indices
-            min_left = self.get_index(30,msg.angle_min,msg.angle_increment)
-            max_left = self.get_index(90,msg.angle_min,msg.angle_increment)
-            min_right = self.get_index(270,msg.angle_min,msg.angle_increment)
-            max_right = self.get_index(330,msg.angle_min,msg.angle_increment)
-            
-            #decide to turn left or right
-            for i in range(min_left,max_left):
-                if not math.isinf(msg.ranges[i]):
-                    distance_left.append(msg.ranges[i])
-                else:
-                    distance_left.append(RANGE_MAX)  #assume no obstacle in that direction
-            for i in range(min_right,max_right):
-                if not math.isinf(msg.ranges[i]):
-                    distance_right.append(msg.ranges[i])
-                else:
-                    distance_right.append(RANGE_MAX)  #assume no obstacle in that direction
+            #decide if turn left or right
             if max(distance_left)>max(distance_right):
-                #self.get_logger().info('turning right')
-                #send.angular.z = ANGULAR_VELOCITY
                 self.state = "left"
                 distance_left.clear()
             else:
-                #send.angular.z = -ANGULAR_VELOCITY
-                #self.get_logger().info('turning left')
                 self.state = "right"
                 distance_right.clear()
 
@@ -129,6 +96,9 @@ class ControllerNode(Node):
         yaw = euler[2]
         self.get_logger().info(
             f'Odom -> x: {position.x:.2f}, y: {position.y:.2f}, Yaw: {yaw:.2f} rad')
+        
+        if self.state == "forward":
+            send.linear.x = LINEAR_VELOCITY
 
         if self.state == "left":
             # Save the starting yaw the first time
@@ -136,7 +106,7 @@ class ControllerNode(Node):
                 self.start_yaw = yaw
             # Calculate the angle difference
             diff = self.normalize_angle(yaw - self.start_yaw)
-            if abs(diff) < math.pi/2 - 0.12:
+            if abs(diff) < math.pi/2 - 0.16:
                 send.angular.z = ANGULAR_VELOCITY
                 self.publisher.publish(send)
             else:
@@ -151,7 +121,7 @@ class ControllerNode(Node):
                 self.start_yaw = yaw
             # Calculate the angle difference
             diff = self.normalize_angle(yaw - self.start_yaw)
-            if abs(diff) < math.pi/2 - 0.12:
+            if abs(diff) < math.pi/2 - 0.16:
                 send.angular.z = -ANGULAR_VELOCITY
                 self.publisher.publish(send)
             else:
@@ -159,6 +129,7 @@ class ControllerNode(Node):
                 send.angular.z = 0.0
                 self.publisher.publish(send)
                 self.start_yaw = None  # Reset for the next rotation
+
 
     def ground_callback(self, msg):
         position = msg.pose.pose.position
